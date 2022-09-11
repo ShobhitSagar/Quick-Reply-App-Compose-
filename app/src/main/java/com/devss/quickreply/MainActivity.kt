e8c1e31053bcc71496636e6aa9390f894f5bdb65
Shobhit Sagar <shobhitsagar109@gmail.com>
2022-09-11 15:24:57 +0530

    Added lotti animation

--- app/src/main/java/com/devss/quickreply/MainActivity.kt
+++ app/src/main/java/com/devss/quickreply/MainActivity.kt
@@ -3,41 +3,90 @@ package com.devss.quickreply
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.*
 import com.devss.quickreply.ui.theme.QuickReplyTheme
 
 class MainActivity : ComponentActivity() {
+    private val demo: Boolean = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            QuickReplyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            Scaffold(topBar = {
+                TopAppBar(title = {
+                    Text(text = "Quick Reply")
+                },
+                actions = { MainMenu() })
+            }) {
+                if (demo) LottieAnimation()
+                else MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp() {
+
+}
+
+@Composable
+fun MainMenu() {
+}
+
+@Composable
+fun LottieAnimation() {
+    var isLottiePlaying by remember {
+        mutableStateOf(true)
+    }
+    var animationSpeed by remember {
+        mutableStateOf(1f)
+    }
+    val composition by rememberLottieComposition(
+        LottieCompositionSpec
+            .RawRes(R.raw.lottie_under_construction)
+    )
+
+    // to control the lottie animation
+    val lottieAnimation by animateLottieCompositionAsState(
+        // pass the composition created above
+        composition,
+        // Iterates Forever
+        iterations = LottieConstants.IterateForever,
+        // Lottie and pause/play
+        isPlaying = isLottiePlaying,
+        // Increasing the speed of change Lottie
+        speed = animationSpeed,
+        restartOnPlay = false
+
+    )
+
+    Column(
+        Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // Pass the composition and the progress state
+        LottieAnimation(
+            composition,
+            lottieAnimation,
+            modifier = Modifier.size(300.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     QuickReplyTheme {
-        Greeting("Android")
+        LottieAnimation()
     }
 }
